fa27bd4d59fb48cc826e6dfdcd7a4d6b5d7af9ca
ST1042699 <ST10472699@rcconnect.edu.za>
2026-09-03 19:19:37 +0200

    db: created table called Roles

--- docs/RaceDay Database.sql
+++ docs/RaceDay Database.sql
@@ -8,6 +8,7 @@ GO
 USE RaceDay;
 GO
 
+--2. created a clean up 
 IF OBJECT_ID('dbo.Results', 'U') IS NOT NULL DROP TABLE dbo.Results;
 IF OBJECT_ID('dbo.Enrolments', 'U') IS NOT NULL DROP TABLE dbo.Enrolments;
 IF OBJECT_ID('dbo.Categories', 'U') IS NOT NULL DROP TABLE dbo.Categories;
@@ -15,3 +16,9 @@ IF OBJECT_ID('dbo.Events', 'U') IS NOT NULL DROP TABLE dbo.Events;
 IF OBJECT_ID('dbo.[User]', 'U') IS NOT NULL DROP TABLE dbo.[User];
 IF OBJECT_ID('dbo.Roles', 'U') IS NOT NULL DROP TABLE dbo.Roles;
 GO
+
+--3. created the first table called roles 
+CREATE TABLE Roles (
+    RoleID INT IDENTITY(1,1) PRIMARY KEY,
+    Rolename VARCHAR(50) NOT NULL UNIQUE
+);
