d3519ab477e406b55f496f4c530b190e7d82da6b
ST1042699 <ST10472699@rcconnect.edu.za>
2026-09-03 20:03:45 +0200

    db: Created table Categories

--- docs/RaceDay Database.sql
+++ docs/RaceDay Database.sql
@@ -1,4 +1,4 @@
---1. create the database 
+
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'RaceDay')
 BEGIN
     CREATE DATABASE RaceDay;
@@ -8,7 +8,7 @@ GO
 USE RaceDay;
 GO
 
---2. created a clean up 
+
 IF OBJECT_ID('dbo.Results', 'U') IS NOT NULL DROP TABLE dbo.Results;
 IF OBJECT_ID('dbo.Enrolments', 'U') IS NOT NULL DROP TABLE dbo.Enrolments;
 IF OBJECT_ID('dbo.Categories', 'U') IS NOT NULL DROP TABLE dbo.Categories;
@@ -17,13 +17,13 @@ IF OBJECT_ID('dbo.[User]', 'U') IS NOT NULL DROP TABLE dbo.[User];
 IF OBJECT_ID('dbo.Roles', 'U') IS NOT NULL DROP TABLE dbo.Roles;
 GO
 
---3. created the first table called roles 
+
 CREATE TABLE Roles (
     RoleID INT IDENTITY(1,1) PRIMARY KEY,
     Rolename VARCHAR(50) NOT NULL UNIQUE
 );
 
--- 4.table called user
+
 CREATE TABLE [User] (
     UserID INT IDENTITY(1,1) PRIMARY KEY,
     RoleID INT NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE [User] (
     CONSTRAINT FK_User_Roles FOREIGN KEY (RoleID) REFERENCES Roles(RoleID)
 );
 
---5. create table events
+
 CREATE TABLE Events (
     EventID INT IDENTITY(1,1) PRIMARY KEY,
     OrganiserID INT NOT NULL,
@@ -44,4 +44,13 @@ CREATE TABLE Events (
     Location VARCHAR(150) NOT NULL,
     CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
     CONSTRAINT FK_Events_User FOREIGN KEY (OrganiserID) REFERENCES [User](UserID)
+);
+
+CREATE TABLE Categories (
+    CategoryID INT IDENTITY(1,1) PRIMARY KEY,
+    EventID INT NOT NULL,
+    CategoryName VARCHAR(50) NOT NULL,
+    DistanceKM DECIMAL(5,2) NOT NULL,
+    EntryFee DECIMAL(10,2) NOT NULL,
+    CONSTRAINT FK_Categories_Events FOREIGN KEY (EventID) REFERENCES Events(EventID) ON DELETE CASCADE
 );
